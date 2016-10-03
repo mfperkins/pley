60463d17a4742b91edb43630f4b74e6bb852eea4
Alfie <alfie_ab@hotmail.co.uk>
2016-10-03 18:04:02 +0100

    second commit

--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -1,7 +1,15 @@
 class ReviewController < ApplicationController
   def new
   end
- 
+
+  def show
+    @review = Review.find(params[:id])
+  end
+
   def create
+    @review = Review.new(rating: params[:rating], comment: params[:comment])
+
+    @review.save
+    redirect_to @review
   end
 end
