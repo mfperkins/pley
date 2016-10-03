815c948305a049d5cde1dff6f2e04ceea0f1d54f
Alfie <alfie_ab@hotmail.co.uk>
2016-10-03 16:53:36 +0100

    closes #11

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'review/new', to: 'review#new'
 
   resources :restaurants
+  resources :review
   root 'home#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
