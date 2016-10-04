ec16d21dfd0d7713bab673ed637e57837ce72d36
Tam <tamborine@Tams-MacBook-Air.local>
2016-10-04 11:10:27 +0100

    closes #22 associations created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161003163311) do
+ActiveRecord::Schema.define(version: 20161004100748) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,8 +25,11 @@ ActiveRecord::Schema.define(version: 20161003163311) do
   create_table "reviews", force: :cascade do |t|
     t.integer  "rating"
     t.text     "comment"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.integer  "restaurant_id"
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id", using: :btree
   end
 
+  add_foreign_key "reviews", "restaurants"
 end
