c78599a718721e74673bad4a34b52a0eaa66cee8
Rosie Allott <rosie.allott@gmail.com>
2016-10-05 16:43:34 +0100

    Closes user can only edit if signed in

--- spec/features/restaurant_feature_spec.rb
+++ spec/features/restaurant_feature_spec.rb
@@ -2,8 +2,7 @@ require 'rails_helper'
 
 describe 'creating restaurant entry', type: :feature do
   it 'creates a new entry in restaurant table' do
-    create_restaurant
-    expect {click_button('Save Restaurant')}.to change(Restaurant, :count).by(1)
+    expect {create_restaurant('Pizzeria')}.to change(Restaurant, :count).by(1)
     expect(page).to have_current_path('/restaurants/1')
     expect(page).to have_content('Pizzeria')
   end
@@ -11,8 +10,7 @@ end
 
 describe 'restaurant list' do
   before(:each) do
-    create_restaurant
-    click_button('Save Restaurant')
+    create_restaurant('Pizzeria')
   end
 
   it 'shows all restaurants on index' do
@@ -22,22 +20,22 @@ describe 'restaurant list' do
 
   it 'should display the average rating for a restaurant' do
     add_review(1)
-    click_button('Submit')
     add_review(3)
-    click_button('Submit')
     expect(page).to have_content 'Rating: 2.0'
   end
 end
 
-describe 'edit or delete a restaurant listing' do
+describe 'edit or delete a restaurant listing if user signed in' do
   before(:each) do
+    user_sign_up
     @restaurant = Restaurant.new({name: "Restaurant", description: "A good restaurant"})
     @restaurant.save
   end
 
   it 'should allow user to update a restaurant' do
-    update_restaurant
-    click_button('Save Restaurant')
+    visit '/restaurants/1'
+    click_link('Edit')
+    create_restaurant('Prophète')
     expect(page).to have_content 'Prophète'
   end
 
@@ -47,12 +45,26 @@ describe 'edit or delete a restaurant listing' do
   end
 end
 
+describe 'edit or delete a restaurant listing if user not signed in' do
+  before(:each) do
+    @restaurant = Restaurant.new({name: "Restaurant", description: "A good restaurant"})
+    @restaurant.save
+  end
+
+  it 'should allow user to update a restaurant' do
+    visit '/restaurants/1'
+    expect(page).not_to have_link('Edit')
+  end
+
+  it 'should allow user to delete a restaurant' do
+    visit '/restaurants/1'
+    expect(page).not_to have_link('Delete')
+  end
+end
+
 describe 'validations' do
   it 'should not allow visitor to enter empty restaurant name' do
-    visit '/restaurants/new'
-    fill_in 'restaurant[name]', with: ''
-    fill_in 'restaurant[description]', with: 'a quite pretentious restaurant'
-    click_button('Save Restaurant')
+    create_restaurant("")
     expect(page).to have_current_path '/restaurants/new'
     expect(page).to have_content "Name can't be blank"
   end
--- spec/features/review_feature_spec.rb
+++ spec/features/review_feature_spec.rb
@@ -1,26 +1,22 @@
 describe 'it creates a review attatched to a restaurant', type: :feature do
 
   before(:each) do
-    create_restaurant
-    click_button('Save Restaurant')
+    create_restaurant("Pizzeria")
   end
 
   it 'it creates a review attatched to a restaurant' do
-    add_review(2)
-    expect {click_button('Submit')}.to change(Review, :count).by(1)
+    expect {add_review(2)}.to change(Review, :count).by(1)
     expect(page).to have_content("Rating: 2 What a wonderful meal we had")
   end
 
   it 'should only allow user a number between 1 and 5' do
     add_review(6)
-    click_button('Submit')
     expect(page).to have_current_path '/restaurants/1/review/new'
     expect(page).to have_content "Rating must be less than or equal to 5"
   end
 
   it 'should not allow user to enter nothing in rating' do
     add_review('')
-    click_button('Submit')
     expect(page).to have_content "Rating is not a number"
   end
 end
