611d32a16bf9074ab250f129f46dfc6f6c6d1185
Alfie <alfie_ab@hotmail.co.uk>
2016-10-05 12:53:20 +0100

    finished

--- spec/features/associating_rest_review_spec.rb
+++
@@ -1,9 +0,0 @@
-describe 'it creates a review attatched to a restaurant', type: :feature do
-  it 'it creates a review attatched to a restaurant' do
-    create_restaurant
-    click_button 'Save Restaurant'
-    add_review(2)
-    expect {click_button('Submit')}.to change(Review, :count).by(1)
-    expect(page).to have_content("Rating: 2 Comment: What a wonderful meal we had")
-  end
-end
