e3b27c514df2af503cc42841fb264a3ad0ed2d8c
Ewan Sheldon <ewansheldon@gmail.com>
2016-10-05 12:58:23 +0100

    closes #53 - associations with user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+
+  has_many :restaurants
+  has_many :reviews
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable <<< HINT HINT
   devise :database_authenticatable, :registerable,
