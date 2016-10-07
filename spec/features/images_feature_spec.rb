bf02e37f701f982ac4532795a797859629d27684
Matthew Perkins <mfperkins@gmail.com>
2016-10-07 11:36:04 +0100

    refactoring

--- spec/features/images_feature_spec.rb
+++ spec/features/images_feature_spec.rb
@@ -1,13 +1,15 @@
 describe 'uploading and viewing images' do
   before(:each) do
     user_sign_up
-    Restaurant.new
-    @user = User.find_by(id: 1)
-    @restaurant = @user.restaurants.create(:name => "Pizzeria", :description => "good", :image => "../public/broccoli.jpg")
-    puts @restaurant
   end
 
   it 'should render image on the page' do
-    expect(true).to equal(false)
+    visit '/'
+    click_link('Add Restaurant')
+    fill_in 'restaurant[name]', with: "Pizzeria"
+    fill_in 'restaurant[description]', with: 'a restaurant that serves pizza pie'
+    page.attach_file('restaurant[image]', Rails.root + 'spec/features/broccoli.jpg')
+    click_button('Save Restaurant')
+    expect(page).to have_xpath("//img[@src='broccoli.jpg']")
   end
 end
