e169db73e324787eb21fc78e84b1300eb5d43943
Alfie <alfie_ab@hotmail.co.uk>
2016-10-07 11:34:34 +0100

    app id still not working

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,5 +1,7 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
+require 'omniauth-facebook'
+
 Devise.setup do |config|
 
   config.omniauth :facebook, 642706932577257, ENV["FB_PLEY_APP_SECRET"]
